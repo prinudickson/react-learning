f112ef25809176baf99a647fc3dd1e9b3caa60d1
Prinu Dickson <prinu.dickson@nl.pwc.com>
2022-05-20 12:37:40 +0200

    final commit in my-app

--- my-app/src/components/Chart/Chart.js
+++ my-app/src/components/Chart/Chart.js
@@ -1,11 +1,13 @@
-import react from "react";
+import React from "react";
 import ChartBar from "./ChartBar";
 import './Chart.css';
 
 const Chart = (props) => {
+    const valueArray = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaxVal = Math.max(...valueArray);
 
     return <div className='chart'>
-        {props.dataPoints.map(dataPoint => <ChartBar value = {dataPoint.value} maxValue = {null} label = {dataPoint.label} key = {dataPoint.id} />)}
+        {props.dataPoints.map(dataPoint => <ChartBar value = {dataPoint.value} maxValue = {totalMaxVal} label = {dataPoint.label} key = {dataPoint.label} />)}
     </div>
 };
 
--- my-app/src/components/Expenses/Expenses.js
+++ my-app/src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -22,6 +23,7 @@ const Expenses = (props) => {
         <div>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeYearFilter={filterChangeHandler} />
+                <ExpensesChart expenses = {filteredExpenses}/>
                 <ExpensesList items={filteredExpenses}/>
             </Card>
             
